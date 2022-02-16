c7b52d4d83cf81d2876f6cede2bcc499401032af
lamnguyen5464 <lamnguyen5464@gmail.com>
2022-02-16 17:22:05 +0700

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyDemo"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/demo/kmm/Platform.kt
@@ -0,0 +1,5 @@
+package com.demo.kmm
+
+expect class Platform() {
+    val platform: String
+}
