70105e54cdc56e3b82b1c41a9cb20bce9517eff7
lamnguyen5464 <lamnguyen5464@gmail.com>
2022-02-18 14:38:26 +0700

    feat: call api request from ios

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,26 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
-
-	var body: some View {
-		Text(greet)
-	}
+    let greet = Greeting().greeting()
+    
+    let test = testDemo()
+ 
+    var body: some View {
+        Text(greet)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-	ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
 }
+
+func testDemo() {
+    let apiRequest = ApiRequest()
+    
+    apiRequest.getCatFact(completionHandler: { res, err in
+        print(res?.fact)
+    })
+}
+ 
