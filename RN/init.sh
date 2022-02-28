c52bfb08bba216200564a5e1470f5dc5de2f7575
lamnguyen5464 <lamnguyen5464@gmail.com>
2022-02-28 16:05:52 +0700

    chore: change script init

--- RN/init.sh
+++ RN/init.sh
@@ -1,8 +1,10 @@
 # clear 
 cd ios && rm -rf Pods && rm -rf Podfile.lock && cd ..
+rm -rf kmmshared
+rm -rf node_modules 
 
-ln -s android/kmmshared kmmshared
-yarn
-adb reverse tcp:8081 tcp:8081
-cd ios && pod install && cd .. ||
+ln -s android/kmmshared kmmshared 
+yarn 
+adb reverse tcp:8081 tcp:8081 
+cd ios && pod install && cd .. 
 yarn start
